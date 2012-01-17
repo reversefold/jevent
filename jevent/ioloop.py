de71b3dc0c56691ddd3706ccaa6467b866541702
Justin Patrin <jpatrin@lolapps.com>
2012-01-16 22:24:26 -0800

    Remove example code

--- jevent/ioloop.py
+++ jevent/ioloop.py
@@ -206,28 +206,5 @@ class ioloop(greenlet):
                     else:
                         log.info("Received POLLOUT for unregistered fd %r, ignoring", fileno)
 
-#              if fileno == serversocket.fileno():
-#                 connection, address = serversocket.accept()
-#                 connection.setblocking(0)
-#                 epoll.register(connection.fileno(), select.EPOLLIN)
-#                 connections[connection.fileno()] = connection
-#                 requests[connection.fileno()] = b''
-#                 responses[connection.fileno()] = response
-#              elif event & select.EPOLLIN:
-#                 requests[fileno] += connections[fileno].recv(1024)
-#                 if EOL1 in requests[fileno] or EOL2 in requests[fileno]:
-#                    epoll.modify(fileno, select.EPOLLOUT)
-#                    print('-'*40 + '\n' + requests[fileno].decode()[:-2])
-#              elif event & select.EPOLLOUT:
-#                 byteswritten = connections[fileno].send(responses[fileno])
-#                 responses[fileno] = responses[fileno][byteswritten:]
-#                 if len(responses[fileno]) == 0:
-#                    epoll.modify(fileno, 0)
-#                    connections[fileno].shutdown(socket.SHUT_RDWR)
-#              elif event & select.EPOLLHUP:
-#                 epoll.unregister(fileno)
-#                 connections[fileno].close()
-#                 del connections[fileno]
-
 coreloop = ioloop()
 coreloop.switch()
